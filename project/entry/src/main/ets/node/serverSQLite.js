52e6f19d9878ba789ce429025f4324a7abf97fed
AkashiAka <1325332015@qq.com>
2024-06-07 09:16:28 +0800

    包下载

--- project/entry/src/main/ets/node/serverSQLite.js
+++ project/entry/src/main/ets/node/serverSQLite.js
@@ -8,7 +8,7 @@ const iconv = require('iconv-lite');
 
 const app = express();
 const port = 3000;
-const csvFilePath = 'E:/E_Consumer_Behavior_System/E_Consumer_Behavior_System/project/A5-master/任务123/居民客户的用电缴费习惯分析1.csv';
+const csvFilePath = 'E:/works/Harmony/E_Consumer_Behavior_System/project/A5-master/任务123/居民客户的用电缴费习惯分析1.csv';
 
 // 初始化SQLite数据库连接
 const sequelize = new Sequelize({
@@ -119,8 +119,8 @@ const readCSVFile = (filePath, storageArray, encoding) => {
 
 // 初始化数据读取
 readCSVAndCalculateStats(csvFilePath);
-readCSVFile('E:/E_Consumer_Behavior_System/E_Consumer_Behavior_System/project/A5-master/任务123/居民客户的用电缴费习惯分析2.csv', csv2Data,'GBK');
-readCSVFile('E:/E_Consumer_Behavior_System/E_Consumer_Behavior_System/project/A5-master/任务123/居民客户的用电缴费习惯分析3.csv', csv3Data,'UTF-8');
+readCSVFile('E:/works/Harmony/E_Consumer_Behavior_System/project/A5-master/任务123/居民客户的用电缴费习惯分析2.csv', csv2Data,'GBK');
+readCSVFile('E:/works/Harmony/E_Consumer_Behavior_System/project/A5-master/任务123/居民客户的用电缴费习惯分析3.csv', csv3Data,'UTF-8');
 // API端点以返回计算的平均值
 app.get('/payment-stats', (req, res) => {
     res.json(paymentStats);
